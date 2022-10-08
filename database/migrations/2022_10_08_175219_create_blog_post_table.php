b5dc519cb4fca6f53ee001278ce6af80e3ece06b
hassan254-prog <thebigstormtech@gmail.com>
2022-10-08 20:58:38 +0300

    init commit

---
+++ database/migrations/2022_10_08_175219_create_blog_post_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_post', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');  // Title of our blog post          
+            $table->text('body');   // Body of our blog post                  
+            $table->timestamps();
+            $table->softDeletes(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_post');
+    }
+};
